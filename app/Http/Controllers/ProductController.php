54f94c6943e01e1116d6b5587bf25c4bc1167706
Fuad_Hashimov <fuadheshimzade@gmail.com>
2024-05-15 21:39:56 +0400

    Get Dynamic Data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(){
-        
 
-        return view('shop.index');
+        $products = Product::all();
+
+        return view('shop.index',compact('products'));
     }
 
-    public function show(){
-        return view('shop.show');
+    public function show($id){
+        $product = Product::FindOrFail($id);
+
+        return view('shop.show',compact('product'));
     }
 
 }
